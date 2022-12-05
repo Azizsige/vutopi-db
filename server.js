aee4838725c89b83605d66eed6563c5571e75e9f
azizsige <kurniawan00azizsige@gmail.com>
2022-12-05 11:59:02 +0700

    test: delete methods 4

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const router = jsonServer.router(data, "dummy");
 // const router = jsonServer.router("db.json");
 // const middlewares = jsonServer.defaults();
 
-// server.use(middlewares);
+server.use(middlewares);
 // Add this before server.use(router)
 // server.use(jsonServer.rewriter({
 //     '/api/*': '/$1',
